4d43adf620faeab5be65d754d39da88215965012
SImon <jolet.simon@yahoo.com>
2019-09-02 11:00:45 +0200

    remove nodemodules

--- dist/React-Pomodorro.e31bb0bc.js
+++ dist/React-Pomodorro.e31bb0bc.js
@@ -119,6 +119,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"index.js":[function(require,module,exports) {
 console.log("hello world");
+console.log("Coucou");
 },{}],"../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +148,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52788" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56622" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
