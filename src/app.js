67c68df42a3778e5d2e2d4e59ab4c14462230329
SImon <jolet.simon@yahoo.com>
2019-09-02 14:15:19 +0200

    Add files tutos parcel

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.css";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Parcel World</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
