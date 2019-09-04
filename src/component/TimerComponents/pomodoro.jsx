5b42e07e4182a0051f19eb933058412bb3cc80a4
SImon <jolet.simon@yahoo.com>
2019-09-04 16:07:52 +0200

    Push fonctionnal timer

--- src/component/TimerComponents/pomodoro.jsx
+++ src/component/TimerComponents/pomodoro.jsx
@@ -1,42 +1,63 @@
 import React, {useState, useEffect} from "react";
 
-const [seconde, setSeconde] = useState(25 * 60);
-const [paused, setPaused] = useState(true);
+export default () => {
+    const [seconde, setSeconde] = useState(25 * 60);
+    const [paused, setPaused] = useState(true);
 
-useEffect(() => {
-    const interval = setInterval(() => {
-        console.log(`${Date.now()} - paused: ${paused}`);
-        if (!paused) {
-            setSeconde(s => s - 1);
-        }
-    }, 1000);
-    return () => {
-        clearInterval(interval);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!paused) {
+                setSeconde(s => s - 1);
+            }
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [paused]);
+
+    const setPlus = () => {
+        setSeconde(seconde + 60);
     };
-}, [paused]);
-
-function startTimer() {
-    setPaused(false);
-}
-function pauseTimer() {
-    setPaused(true);
-}
-function resetTimer() {
-    setPaused(true);
-    setSeconde(25 * 60);
-}
-return (
-    <div>
-        {`${Math.floor(seconde / 60)
-            .toString()
-            .padStart(2, "0")}:${`${seconde % 60}`
-            .toString()
-            .padStart(2, "0")}`}
-        <button type={"button"} onClick={paused ? startTimer : pauseTimer}>
-            {paused ? "Start" : "Pause"}
-        </button>
-        <button type={"button"} onClick={resetTimer}>
-            {"Reset"}
-        </button>
-    </div>
-);
+    const setMoins = () => {
+        setSeconde(seconde - 60);
+    };
+    const startTimer = () => {
+        setPaused(false);
+    };
+    const pauseTimer = () => {
+        setPaused(true);
+    };
+    const resetTimer = () => {
+        setPaused(true);
+        setSeconde(25 * 60);
+    };
+    return (
+        <div>
+            <div class={"timer"}>
+                {`${Math.floor(seconde / 60)
+                    .toString()
+                    .padStart(2, "0")}:${`${seconde % 60}`
+                    .toString()
+                    .padStart(2, "0")}`}
+            </div>
+            <div className="bouton">
+                {" "}
+                <button id={"plusBtn"} type={"button"} onClick={setPlus}>
+                    {"Plus"}
+                </button>
+                <button
+                    id={"startStopBtn"}
+                    type={"button"}
+                    onClick={paused ? startTimer : pauseTimer}>
+                    {paused ? "Start" : "Pause"}
+                </button>
+                <button id={"resetBtn"} type={"button"} onClick={resetTimer}>
+                    {"Reset"}
+                </button>
+                <button id={"moinsBtn"} type={"button"} onClick={setMoins}>
+                    {"Moins"}
+                </button>
+            </div>
+        </div>
+    );
+};
